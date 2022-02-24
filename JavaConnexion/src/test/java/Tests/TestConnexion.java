0b07ac7d322e70b19ea13b9363eba728a98ff156
Elias JALAL <elias.jalal@edu.itescia.fr>
2022-02-24 12:00:30 +0100

    feature/Tests_Amelioration : Amélioration du test TestConnexion

--- JavaConnexion/src/test/java/Tests/TestConnexion.java
+++ JavaConnexion/src/test/java/Tests/TestConnexion.java
@@ -5,6 +5,10 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.io.IOException;
 
 import org.json.simple.parser.ParseException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 import org.openjfx.javafx_archetype_simple.ConnexionController;
 import org.openjfx.javafx_archetype_simple.JsonController;
@@ -13,6 +17,29 @@ import org.openjfx.javafx_archetype_simple.PrimaryController;
 
 class TestConnexion {
 	String path = "src/test/resources/json/personne.json";
+	ConnexionController connexion = new ConnexionController();
+
+    @BeforeClass
+    public static void firstSetUp() {
+        System.out.println("Début des tests de la classe ConnexionController.");
+    }
+    
+    @Before
+    public void setUp() throws Exception {
+        this.connexion = new ConnexionController();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    	System.out.println("Fin du test");
+        //this.connexion.shutdown();
+    }
+
+    @AfterClass
+    public static void finalTearDown() throws Exception {
+    	System.out.println("Fin des tests de la classe ConnexionController.");
+        //this.connexion.shutdown();
+    }
 	
 	/**
 	 * Permet de vérifier que la connexion Admin est ok
@@ -21,12 +48,11 @@ class TestConnexion {
 	 * @throws IOException
 	 */
 	@Test
-	void testConnexionAdminOk() throws ParseException, java.text.ParseException, IOException {
+	public void testConnexionAdminOk() throws ParseException, java.text.ParseException, IOException {
 		JsonController.ReadJSON(path);
-		ConnexionController connexion = new ConnexionController();
-		connexion.setId("blandine.danteuille");
-		connexion.setMdp("azertyA");
-		assertEquals("ADMIN",connexion.connexion());
+		this.connexion.setId("blandine.danteuille");
+		this.connexion.setMdp("azertyA");
+		assertEquals("ADMIN", this.connexion.connexion());
 	}
 	
 	/**
@@ -36,12 +62,11 @@ class TestConnexion {
 	 * @throws IOException
 	 */
 	@Test
-	void testConnexionUserOk() throws ParseException, java.text.ParseException, IOException {
+	public void testConnexionUserOk() throws ParseException, java.text.ParseException, IOException {
 		JsonController.ReadJSON(path);
-		ConnexionController connexion = new ConnexionController();
-		connexion.setId("elias.jalal");
-		connexion.setMdp("azertyU");
-		assertEquals("USER",connexion.connexion());
+		this.connexion.setId("elias.jalal");
+		this.connexion.setMdp("azertyU");
+		assertEquals("USER", this.connexion.connexion());
 	}
 	
 	/**
@@ -51,12 +76,11 @@ class TestConnexion {
 	 * @throws IOException
 	 */
 	@Test
-	void testConnexionKO() throws ParseException, java.text.ParseException, IOException {
+	public void testConnexionKO() throws ParseException, java.text.ParseException, IOException {
 		JsonController.ReadJSON(path);
-		ConnexionController connexion = new ConnexionController();
-		connexion.setId("blandine.danteuille");
-		connexion.setMdp("azertyU");
-		assertEquals("NON",connexion.connexion());
+		this.connexion.setId("blandine.danteuille");
+		this.connexion.setMdp("azertyU");
+		assertEquals("NON", this.connexion.connexion());
 	}
 	
 	/**
@@ -66,12 +90,11 @@ class TestConnexion {
 	 * @throws IOException
 	 */
 	@Test
-	void testConnexionVideId() throws ParseException, java.text.ParseException, IOException {
+	public void testConnexionVideId() throws ParseException, java.text.ParseException, IOException {
 		JsonController.ReadJSON(path);
-		ConnexionController connexion = new ConnexionController();
-		connexion.setId("");
-		connexion.setMdp("azertyU");
-		assertEquals("NON",connexion.connexion());
+		this.connexion.setId("");
+		this.connexion.setMdp("azertyU");
+		assertEquals("NON", this.connexion.connexion());
 	}
 	
 	/**
@@ -81,12 +104,11 @@ class TestConnexion {
 	 * @throws IOException
 	 */
 	@Test
-	void testConnexionVideMdp() throws ParseException, java.text.ParseException, IOException {
+	public void testConnexionVideMdp() throws ParseException, java.text.ParseException, IOException {
 		JsonController.ReadJSON(path);
-		ConnexionController connexion = new ConnexionController();
-		connexion.setId("blandine.danteuille");
-		connexion.setMdp("");
-		assertEquals("NON",connexion.connexion());
+		this.connexion.setId("blandine.danteuille");
+		this.connexion.setMdp("");
+		assertEquals("NON", this.connexion.connexion());
 	}
 	
 	/**
@@ -96,11 +118,10 @@ class TestConnexion {
 	 * @throws IOException
 	 */
 	@Test
-	void testConnexionVide() throws ParseException, java.text.ParseException, IOException {
+	public void testConnexionVide() throws ParseException, java.text.ParseException, IOException {
 		JsonController.ReadJSON(path);
-		ConnexionController connexion = new ConnexionController();
-		connexion.setId("");
-		connexion.setMdp("");
-		assertEquals("NON",connexion.connexion());
+		this.connexion.setId("");
+		this.connexion.setMdp("");
+		assertEquals("NON", this.connexion.connexion());
 	}
 }
